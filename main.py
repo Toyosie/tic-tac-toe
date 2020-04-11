be1dd2f1b18013381da0b00bfde0bceda529198b
Toyosie <toyositaadetolata@gmail.com>
2020-04-11 19:21:59 +0100

    added images and rezised

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 XO = 'x'
 winner = None
 draw = False
-width = 400
+width = 500
 height = 400
 white = (255, 255, 255)
 line_colour = (10, 10, 10)
@@ -20,3 +20,16 @@ fps = 30
 CLOCK = pg.time.Clock()
 screen = pg.display.set_mode((width,height+100),0,32)
 pg.display.set_caption("tIc tAc tOe")
+
+#to load the images
+opening = pg.image.load('open.png')
+ximg = pg.image.load('X.png')
+oimg = pg.image.load('O.png')
+
+#resizing the images
+ximg = pg.transform.scale(ximg, (80,80))
+oimg = pg.transform.scale(oimg,(80,80))
+opening = pg.transform.scale(opening, (width,height+100))
+
+
+
