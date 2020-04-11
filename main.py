181e957b5bac8324fbf1b59e833aa337ac9c66b3
Toyosie <toyositaadetolata@gmail.com>
2020-04-11 12:23:27 +0100

    global variables

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import pygame
+import pygame as pg, sys
+from pygame.locals import *
+import time
+
+#initializing global variables
+XO = 'x'
+winner = None
+draw = False
+width = 400
+height = 400
+white = (255, 255, 255)
+line_colour = (10, 10, 10)
+
+# 3x3 board
+TTT = [[None]*3,[None]*3,[None]*3]
 
