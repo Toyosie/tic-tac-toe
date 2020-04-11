531b8898ec6f7190953421c01781ce261d5910ba
Toyosie <toyositaadetolata@gmail.com>
2020-04-11 18:37:03 +0100

    initialized the pgame window

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ line_colour = (10, 10, 10)
 # 3x3 board
 TTT = [[None]*3,[None]*3,[None]*3]
 
+#to initialize a pygame window
+pg.init()
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width,height+100),0,32)
+pg.display.set_caption("tIc tAc tOe")
